d1764621a65c4cf06103fc307e528eec639ca43f
Asma Khan <asmakh1052@gmail.com>
2026-08-02 23:01:58 +0500

    Delete src directory

--- src/db/migrate.js
+++
@@ -1,4 +0,0 @@
-const { ensureSchema, DB_PATH } = require('./connection');
-
-ensureSchema();
-console.log(`Schema applied successfully at ${DB_PATH}`);
--- src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { summary, trends } = require('../controllers/dashboardController');
-
-const router = express.Router();
-
-router.get('/summary', summary);
-router.get('/trends', trends);
-
-module.exports = router;
